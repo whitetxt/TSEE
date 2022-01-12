85ad2771491dcf9cea0452fea98143f846903f2d
whitetxt <54776821+whitetxt@users.noreply.github.com>
2022-01-12 15:48:45 +0000

    Started work on UI system

--- src/tsee_ui.c
+++ src/tsee_ui.c
@@ -1,6 +1,11 @@
 #include "include/tsee.h"
 
 bool TSEEInitUI(TSEE *tsee) {
-	
+	if (!tsee->init->text) {
+		TSEEError("Attempted to init TSEEUI without initialising TSEEText");
+		return false;
+	}
+	tsee->ui->toolbar = TSEEArrayCreate();
+	tsee->init->text = true;
 	return true;
 }
