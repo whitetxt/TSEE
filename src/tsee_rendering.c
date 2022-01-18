80cb24f424fdbf0ff0d5a4e1006ca7eb03a35e91
whitetxt <54776821+whitetxt@users.noreply.github.com>
2022-01-18 11:19:09 +0000

    Replaced printf's with appropriate TSEE logs

--- src/tsee_rendering.c
+++ src/tsee_rendering.c
@@ -16,14 +16,14 @@ bool TSEEInitRendering(TSEE *tsee) {
 
 	tsee->window->window = SDL_CreateWindow("No window title set: Call TSEESetWindowTitle", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, tsee->window->width, tsee->window->height, SDL_WINDOW_SHOWN);
 	if (tsee->window->window == NULL) {
-		printf("Window could not be created! SDL_Error: %s\n", SDL_GetError());
+		TSEEError("Window could not be created! SDL_Error: %s\n", SDL_GetError());
 		SDL_Quit();
 		return false;
 	}
 
 	tsee->window->renderer = SDL_CreateRenderer(tsee->window->window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
 	if (tsee->window->renderer == NULL) {
-		printf("Renderer could not be created! SDL_Error: %s\n", SDL_GetError());
+		TSEEError("Renderer could not be created! SDL_Error: %s\n", SDL_GetError());
 		SDL_DestroyWindow(tsee->window->window);
 		SDL_Quit();
 		return false;
