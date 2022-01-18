7ad93ac532ca946a42680b0a3b46741b931c21a5
whitetxt <54776821+whitetxt@users.noreply.github.com>
2022-01-18 11:29:36 +0000

    Started work on saving maps

--- src/tsee_map.c
+++ src/tsee_map.c
@@ -29,7 +29,7 @@ bool TSEELoadMap(TSEE *tsee, char *fn) {
 	// Read type of texture storage
 	// 0 = Texture path stored
 	// 1 = Texture stored in file
-	int textureStorageType = -1;
+	Uint8 textureStorageType = -1;
 	fread(&textureStorageType, sizeof(textureStorageType), 1, fp);
 	if (textureStorageType == 0) {
 		for (int i = 0; i < numTextures; i++) {
@@ -96,3 +96,58 @@ bool TSEELoadMap(TSEE *tsee, char *fn) {
 	}
 	return true;
 }
+
+bool saveMap(TSEE *tsee, char *fn) {
+	FILE *fp = fopen(fn, "wb");
+	if (fp == NULL) {
+		TSEEError("Failed to open map file (%s)\n", fn);
+		return false;
+	}
+	// Write the map header
+	char mapName[32] = "Test Map";
+	char mapAuthor[64] = "Test Author";
+	char mapVersion[8] = "1.0";
+	char mapDescription[512] = "Test Description";
+	fwrite(&mapName, sizeof(mapName), 1, fp);
+	fwrite(&mapAuthor, sizeof(mapAuthor), 1, fp);
+	fwrite(&mapVersion, sizeof(mapVersion), 1, fp);
+	fwrite(&mapDescription, sizeof(mapDescription), 1, fp);
+	// Write the gravity
+	fwrite(&tsee->world->gravity, sizeof(tsee->world->gravity), 1, fp);
+	// Write the number of textures
+	int numTextures = tsee->textures->size;
+	fwrite(&numTextures, sizeof(numTextures), 1, fp);
+	// Write type of texture storage
+	// 0 = Texture path stored
+	// 1 = Texture stored in file
+	Uint8 textureStorageType = 1;
+	fwrite(&textureStorageType, sizeof(textureStorageType), 1, fp);
+	// Write the textures
+	if (textureStorageType == 0) {
+		return false;
+	} else if (textureStorageType == 1) {
+		SDL_Texture* target = SDL_GetRenderTarget(tsee->window->renderer);
+		for (int i = 0; i < numTextures; i++) {
+			TSEE_Texture *texture = TSEEArrayGet(tsee->textures, i);
+			// Get the size of the texture
+			SDL_SetRenderTarget(tsee->window->renderer, texture);
+
+			SDL_Surface *surface = SDL_CreateRGBSurface(0, texture->rect.w, texture->rect.h, 32, 0, 0, 0, 0);
+			SDL_RenderReadPixels(tsee->window->renderer, NULL, surface->format->format, surface->pixels, surface->pitch);
+			IMG_SavePNG();
+			fwrite(&textureSize, sizeof(textureSize), 1, fp);
+			// Open up a stream for SDL to read the surface from
+			SDL_RWops *stream = SDL_RWFromMem(fp, textureSize);
+			if (!stream) { // If the stream couldn't be opened
+				TSEEError("Failed to create SDL_RWops from map file (%s)\n", SDL_GetError());
+				return false;
+			}
+			// Read the surface and convert to a texture
+			SDL_SavePNG_RW(surface, stream, 0);
+			SDL_FreeSurface(surface);
+			SDL_RWclose(stream);
+		}
+		SDL_SetRenderTarget(tsee->window->renderer, target);
+	}
+	return true;
+};
