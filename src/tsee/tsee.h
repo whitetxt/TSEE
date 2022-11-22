9da629ac8973f5b10388621a3826a8a73b5598de
whitetxt <54776821+whitetxt@users.noreply.github.com>
2022-11-22 15:56:23 +0000

    Fixed clang-format fucking over my includes >:((((

--- src/tsee/tsee.h
+++ src/tsee/tsee.h
@@ -5,37 +5,37 @@
 #include <SDL2/SDL_image.h>
 #include <SDL2/SDL_ttf.h>
 #include <libfyaml.h>
-#include <math.h>
-#include <stdbool.h>
-#include <stdint.h>
 #include <stdio.h>
-#include <stdlib.h>
+#include <stdbool.h>
 #include <string.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <math.h>
 
 #include "ext/tinyfiledialogs.h"
 
 // Include TSEE types
-#include "UI/tsee_ui_types.h"
-#include "events/tsee_event_types.h"
-#include "image/tsee_image_types.h"
-#include "maps/tsee_map_types.h"
-#include "objects/tsee_object_types.h"
-#include "physics/tsee_physics_types.h"
-#include "player/tsee_player_types.h"
-#include "resources/tsee_resources_types.h"
 #include "util/tsee_util_types.h"
+#include "resources/tsee_resources_types.h"
+#include "image/tsee_image_types.h"
+#include "objects/tsee_object_types.h"
+#include "UI/tsee_ui_types.h"
+#include "maps/tsee_map_types.h"
+#include "events/tsee_event_types.h"
+#include "player/tsee_player_types.h"
+#include "physics/tsee_physics_types.h"
 
 #include "main/tsee_types.h"
 
 // Include TSEE functions
-#include "UI/tsee_ui_functions.h"
-#include "events/tsee_event_functions.h"
-#include "image/tsee_image_functions.h"
-#include "maps/tsee_map_functions.h"
-#include "objects/tsee_object_functions.h"
-#include "physics/tsee_physics_functions.h"
-#include "player/tsee_player_functions.h"
-#include "resources/tsee_resources_functions.h"
 #include "util/tsee_util_functions.h"
+#include "resources/tsee_resources_functions.h"
+#include "image/tsee_image_functions.h"
+#include "objects/tsee_object_functions.h"
+#include "UI/tsee_ui_functions.h"
+#include "maps/tsee_map_functions.h"
+#include "events/tsee_event_functions.h"
+#include "player/tsee_player_functions.h"
+#include "physics/tsee_physics_functions.h"
 
 #include "main/tsee_functions.h"
